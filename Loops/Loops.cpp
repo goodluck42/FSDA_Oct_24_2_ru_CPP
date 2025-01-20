cd371f2ca6b34454a5971435136bae9bf259a798
Alex <xrlogic1@gmail.com>
2025-01-20 17:12:18 +0400

    *

---
+++ Loops/Loops.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+// range based for
+
+int main(int argc, char* argv[])
+{
+    // int i = 0;
+    //
+    // while (i < 10)
+    // {
+    //     std::cout << i << '\n';
+    //
+    //     i++;
+    // }
+
+    // bool flag = true;
+    // int i = 0;
+    //
+    // while (i < 10 && flag)
+    // {
+    //     
+    // }
+    //
+    // while (flag)
+    // {
+    //     while (flag)
+    //     {
+    //         if (...)
+    //         {
+    //             flag = false;
+    //         }
+    //         
+    //     }
+    //
+    //     // ...
+    // }
+    
+    // int a = 10;
+    //
+    // std::cout << (a++ + ++a) << '\n';
+    // std::cout << a << '\n';
+
+
+    // int i = 0;
+    //
+    // do 
+    // {
+    //     std::cout << i++ << '\n';
+    // } while (false);
+
+    
+    
+    // int i = 0;
+    //
+    // while (i < 10)
+    // {
+    //    i++;
+    //
+    //  //   std::cout << i << "\n";
+    // }
+    //
+    // std::cout << i;
+
+
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     std::cout << i << '\n';
+    // }
+    //
+    // for (;;)
+    // {
+    //     
+    // }
+
+   // std::cout << i;
+
+
+    for (int i = 0; i < 5; std::cout << i++ << '\n');
+    
+    return 0;
+}
+
+void foo(int* i)
+{
+    
+}
