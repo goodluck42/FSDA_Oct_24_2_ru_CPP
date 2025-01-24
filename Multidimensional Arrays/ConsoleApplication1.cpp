319727f7736bea1de490e220aabdd5d714108047
Alex <xrlogic1@gmail.com>
2025-01-24 17:31:27 +0400

    Multidimensional arrays

---
+++ Multidimensional Arrays/ConsoleApplication1.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+
+
+enum Cell
+{
+    Empty = 0,
+    Cross = 1,
+    Zero = -1
+};
+
+
+int main()
+{
+    const int rows = 3;
+    const int cols = 3;
+
+    //  0   1   2
+    // [X] [ ] [ ] 0
+    // [ ] [X] [ ] 1
+    // [ ] [ ] [X] 2
+
+
+    int matrix[rows][cols]{};
+
+    // true - X
+    // false - 0
+    bool player = true;
+    bool isWin = false;
+
+    while (true)
+    {
+        if (isWin)
+        {
+
+            std::cout << "Game over" << '\n';
+            break;
+        }
+        
+        int i; // row
+        int j; // col
+
+        if (player)
+        {
+            std::cout << "Qadir (X) move! \n";
+        }
+        else
+        {
+            std::cout << "Ruslan (O) move! \n";
+        }
+
+        std::cout << "Enter row col -> ";
+        std::cin >> i >> j;
+
+        if (matrix[i][j] != Empty)
+        {
+            std::cout << "Cell is not empty!" << '\n';
+
+            continue;
+        }
+
+        // Place a sign (X or 0)
+        if (player)
+        {
+            matrix[i][j] = Cross;
+        }
+        else
+        {
+            matrix[i][j] = Zero;
+        }
+
+        // change player
+        player = !player;
+
+        // Print matrix
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                char c;
+
+                switch (matrix[i][j])
+                {
+                default:
+                case Empty:
+                    c = ' ';
+                    break;
+                case Cross:
+                    c = 'X';
+                    break;
+                case Zero:
+                    c = '0';
+                }
+
+                std::cout << '|' << c << "| ";
+            }
+
+            std::cout << '\n';
+        }
+
+        // check diag
+        // 0 0, 1 1, 2 2
+        // if (matrix[0][0] == matrix[1][1]
+        //     && matrix[1][1] == matrix[2][2])
+        // {
+        //     isWin = true;
+        // }
+
+        // check alt diag
+        // 0 2, 1 1, 2 0
+        // if (matrix[0][2] == matrix[1][1]
+        //     && matrix[1][1] == matrix[2][0])
+        // {
+        //     isWin = true;
+        // }
+        //
+        // // 0 0, 0 1, 0 2
+        // if (matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2])
+        // {
+        //     isWin = true;
+        // }
+        //
+        // // 1 0, 1 1, 1 2
+        // if (matrix[1][0] == matrix[1][1] && matrix[1][1] == matrix[1][2])
+        // {
+        //     isWin = true;
+        // }
+        //
+        //
+        // // 2 0, 2 1, 2 2
+        // if (matrix[2][0] == matrix[2][1] && matrix[2][1] == matrix[2][2])
+        // {
+        //     isWin = true;
+        // }
+        // // 0 0, 1 0, 2 0
+        // if (matrix[0][0] == matrix[1][0] && matrix[1][0] == matrix[2][0])
+        // {
+        //     isWin = true;
+        // }
+        //
+        // // 0 1, 1 1, 2 1
+        // if (matrix[0][1] == matrix[1][1] && matrix[1][1] == matrix[2][1])
+        // {
+        //     isWin = true;
+        // }
+        //
+        // // 0 2, 1 2, 2 2
+        // if (matrix[0][2] == matrix[1][2] && matrix[1][2] == matrix[2][2])
+        // {
+        //     isWin = true;
+        // }
+    }
+    system("pause");
+
+    return 0;
+}
