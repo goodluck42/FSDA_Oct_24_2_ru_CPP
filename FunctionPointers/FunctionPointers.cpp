ede3276dc20367dcd30829e8e6f9e160bb8e613c
Alex <xrlogic1@gmail.com>
2025-02-14 17:46:23 +0400

    CPP: Function pointers UPD

--- FunctionPointers/FunctionPointers.cpp
+++ FunctionPointers/FunctionPointers.cpp
@@ -33,55 +33,55 @@ typedef int (*action)(short, short);
 // using int64 = long long;
 //using action = int (*)(short, short);
 
+bool less(int a, int b)
+{
+    return a < b;
+}
+
+bool greater(int a, int b)
+{
+    return a > b;
+}
+
+void bubble_sort(int* arr, int length, bool(*comparator)(int, int))
+{
+    // ???
+}
+
+
 int main(int argc, char* argv[])
 {
-    // int (*func)(short, short) = mult;
-    action func = nullptr;
-    char op;
-
-    std::cout << "Enter operation type:";
-    std::cin >> op;
-
-    switch (op)
-    {
-    case '+':
-        func = sum;
-        break;
-    case '*':
-        func = mult;
-        break;
-    }
-
-    short a, b;
-
-    std::cout << "Enter two numbers:";
-    std::cin >> a >> b;
-
-    if (func != nullptr)
-    {
-        int result = func(a, b);
-
-        std::cout << result << '\n';
-    }
+    bubble_sort(nullptr, 0, greater);
     
-   
-
-    // int result = func(10, 20);
-
-    // std::cout << result << '\n';
-
-
-    // int arr[3]{10, 20, 30};
+    int (*func)(short, short) = mult;
+    //action func = nullptr;
+    // char op;
     //
-    // print(arr, 3);
-
-
-    // int arr_2[3]{10, 20, 30};
-    // int* arr = new int[3000]{10, 20, 30};
+    // std::cout << "Enter operation type:";
+    // std::cin >> op;
     //
-    // std::cout << *arr << '\n';
+    // switch (op)
+    // {
+    // case '+':
+    //     func = sum;
+    //     break;
+    // case '*':
+    //     func = mult;
+    //     break;
+    // }
     //
-    // delete[] arr;
+    // short a, b;
+    //
+    // std::cout << "Enter two numbers:";
+    // std::cin >> a >> b;
+    //
+    // if (func != nullptr)
+    // {
+    //     int result = func(a, b);
+    //
+    //     std::cout << result << '\n';
+    // }
+    
 
     return 0;
 }
